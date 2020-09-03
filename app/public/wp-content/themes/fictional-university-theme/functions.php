d6a7f0c2fa2585811ff96e64d1d3144fff7fe750
Ramon Audain <raudain123@gmail.com>
2020-09-02 19:35:29 -0700

    Advoiding Caching during development

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,11 +1,13 @@
 <?php 
 
 function university_files() {
-    wp_enqueue_script("slider", get_theme_file_uri("/js/scripts-bundled.js"), NULL, "1.0", true);
+    // brings in JavaScript file
+    wp_enqueue_script("slider", get_theme_file_uri("/js/scripts-bundled.js"), NULL, microtime(), true);
+    // brings in googlefonts and font-awesome
     wp_enqueue_style("googlefonts", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style("font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
     // get_stylesheet_uri() gets css file
-    wp_enqueue_style("main_styles", get_stylesheet_uri());
+    wp_enqueue_style("main_styles", get_stylesheet_uri(), NULL, microtime());
 
 }
 
