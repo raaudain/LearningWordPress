3c3bf874116f88ca19260fb099965c78288a4d39
Ramon Audain <raudain123@gmail.com>
2020-09-02 17:46:13 -0700

    The WordPress Loop

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,23 +1,11 @@
 <?php  
 
-$names = ["Ramon", "Tim", "Sarah", "John", "Tech N9ne"];
-
-// foreach ($names as $x) {
-//     if (strpos($x, "a")) {
-
-//         echo $x;
-//     }
-// }
-// if (names)
-
-$count = 0;
-
-function printNames() {
-    global $names, $count;
-    while ($names[$count]) {
-        echo "<br>" . $names[$count];
-        $count++;
-    }
+while(have_posts()) {
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <p><?php the_content(); ?></p>
+    <hr>
+    <?php  
 }
 
 ?>
@@ -31,7 +19,6 @@ function printNames() {
     <title>Document</title>
 </head>
 <body>
-    <?php bloginfo("name"); ?>
-    <h1><?php printNames(); ?></h1>
+    <h1><?php ?></h1>
 </body>
 </html>
