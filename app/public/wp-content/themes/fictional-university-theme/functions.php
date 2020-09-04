9a30feb9659fface4e1ac8650a9fa81a33af6df0
Ramon Audain <raudain123@gmail.com>
2020-09-04 14:53:35 -0700

    Interior page Template

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -14,4 +14,11 @@ function university_files() {
 // Runs university_files()
 add_action("wp_enqueue_scripts", "university_files");
 
+// Changes page title
+function university_features() {
+    add_theme_support("title-tag");
+}
+
+add_action("after_setup_theme", "university_features");
+
 ?>
