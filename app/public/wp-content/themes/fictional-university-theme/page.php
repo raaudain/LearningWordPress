0b93e4668097bae3a2cc1e1a1f693bfdd52d1f4a
Ramon Audain <raudain123@gmail.com>
2020-09-04 19:35:09 -0700

    Menu of Child Page Links

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -1,52 +1,76 @@
-<?php  
+<?php
 
 get_header();
 
 // page.php is for a single page.
-while(have_posts()) {
+while (have_posts()) {
     the_post(); ?>
-    
-    <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("images/ocean.jpg"); ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DON'T FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>  
-  </div>
 
-  <div class="container container--narrow page-section">
-    <?php 
-     $the_parent = wp_get_post_parent_id(get_the_ID());
-     // If child page display...
-     if ($the_parent) {
-         ?>
-        <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($the_parent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($the_parent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("images/ocean.jpg"); ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p>DON'T FORGET TO REPLACE ME LATER</p>
+            </div>
         </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+        <?php
+        $the_parent = wp_get_post_parent_id(get_the_ID());
+        // If child page display...
+        if ($the_parent) {
+        ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($the_parent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($the_parent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+            </div>
 
         <?php
 
-     }
+        }
 
-    ?>
+        ?>
 
-    
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div> -->
+        <?php 
+            // get_pages returns pages in memory
+            $hasChild = get_pages([
+                "child_of" => get_the_ID() // Checks if the page has children
+            ]);
 
-    <div class="generic-content">
-      <?php the_content(); ?>
+            if ($the_parent or $hasChild) { ?>
+
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?php echo get_permalink($the_parent); ?>"><?php echo get_the_title($the_parent); ?></a></h2>
+                <ul class="min-list">
+                    <?php
+                    if ($the_parent) {
+                        $findChildrenOf = $the_parent;
+                    } else {
+                        $findChildrenOf = get_the_ID();
+                    }
+
+                    wp_list_pages([
+                        "title_li" => NULL,
+                        "child_of" => $findChildrenOf,
+                        "sort_column" => "menu_order" // Sorts links based on number in admin. By default links are orderd alphabet
+                    ]);
+                    ?>
+                </ul>
+            </div>
+
+
+        <?php
+        }
+
+        ?>
+
+        <div class="generic-content">
+            <?php the_content(); ?>
+        </div>
+
     </div>
-
-  </div>
-    <?php  
+<?php
 }
 
 get_footer();
