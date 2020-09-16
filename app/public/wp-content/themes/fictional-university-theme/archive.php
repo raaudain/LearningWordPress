06653242128186a5dd7a1a2055a490a7a0a2c166
Ramon Audain <raudain123@gmail.com>
2020-09-15 23:06:00 -0700

    Using pageBanner function in files

--- app/public/wp-content/themes/fictional-university-theme/archive.php
+++ app/public/wp-content/themes/fictional-university-theme/archive.php
@@ -1,20 +1,10 @@
-<?php get_header(); ?>
-
-<div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("images/ocean.jpg"); ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">
-                <!-- If statements to display category and author archive names -->
-                <!-- <?php if (is_category()) single_cat_title(); if (is_author()) echo "Posts by " . get_the_author(); ?> -->
-
-                <!-- New, easier way to display archive title -->
-                <?php the_archive_title() ?>
-            </h1>
-            <div class="page-banner__intro">
-                <p><?php the_archive_description(); ?></p>
-            </div>
-        </div>
-    </div>
+<?php
+get_header(); 
+pageBanner([
+    "title" => get_the_archive_title(),
+    "subtitle" => get_the_archive_description()
+]);
+?>
 
 <div class="container container--narrow page-section">
     <?php 
